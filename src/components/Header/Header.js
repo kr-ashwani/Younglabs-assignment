64e70e23bbdc74eb4277420c250e8b64d0e1dfbc
Ashwani Kumar <ashwanikr160@gmail.com>
2022-07-18 09:00:37 +0530

    first-commmit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,11 +8,9 @@ const Header = () => {
   useEffect(() => {
     if (!listRef.current) return;
     function moveUnderLine(e) {
-      underLineRef.current.style.transform = `translateX(calc(${
-        e.target.dataset.num * 100
-      }% + ${e.target.dataset.num * 10}px)) scaleX(${
-        e.target.offsetWidth / underLineRef.current.offsetWidth
-      })`;
+      underLineRef.current.style.transform = `translateX(${
+        e.target.offsetLeft
+      }px) scaleX(${e.target.offsetWidth / underLineRef.current.offsetWidth})`;
     }
     Array.from(listRef.current.children).forEach((elem) => {
       elem.addEventListener('mouseover', moveUnderLine);
@@ -30,9 +28,9 @@ const Header = () => {
       </div>
       <div className="nav">
         <ul ref={listRef}>
-          <li data-num="0">Product</li>
-          <li data-num="1">Download</li>
-          <li data-num="2.1">Pricing</li>
+          <li>Product</li>
+          <li>Download</li>
+          <li>Pricing</li>
         </ul>
         <div ref={underLineRef} className="underLine"></div>
       </div>
